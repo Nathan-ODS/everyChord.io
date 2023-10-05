88038e1a46784e522c5fbc2a95efb92d740ff3de
D0sal <nathan.oliveira.da.silva.pro@gmail.com>
2023-10-05 13:24:22 +0200

    better layout for notes

--- client/src/components/ChordContent/ChordSummary/ChordSummary.jsx
+++ client/src/components/ChordContent/ChordSummary/ChordSummary.jsx
@@ -3,17 +3,20 @@ import "./ChordSummary.css";
 
 const ChordSummary = ({ activeNotes, activeMidiChord, activeChordLabel }) => {
 
-    return (
-        <div className="chord-summary">
-            <div className="label">
-                <span>{activeChordLabel}</span>
-            </div>
-            <div className="notes">
-                <span>{activeNotes}</span>
-                <span>{activeMidiChord}</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="chord-summary">
+      <div className="label">
+        <span>{activeChordLabel}</span>
+      </div>
+      <div className="notes">
+        {activeNotes?.map((note, index) => (
+            <span key={index}>
+              {note}
+            </span>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ChordSummary;
