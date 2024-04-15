f4da35b7555956ec65daf7b41ba9378eddf14b98
Nathan-ODS <nathan.oliveiradasilva@skello.io>
2024-04-15 21:25:15 +0200

    1.0.0

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 - This app will display a piano and chords.
 You can :
 - play the piano and get the chords you're currently playing
-- search for a chord via input or filters
-- select a chord and play it
+- search for a chord using the Interactive chord wheel
 
 # What is what
 - `/server` is the express server (backend)
 - `/client` is the React app (frontend)
+
 # How to use
-- open terminal in root then `npm start` to start your express server
-- open side-terminal then `cd client` then `npm start` to start your React app
+### The API
+```bash
+cd /server
+npm install
+npm start
+```
+
+### The React APP
+```bash
+cd /client
+npm install
+npm start
+```
 
 # External stuffed used
 - [Piano React Component](https://www.kevinqi.com/react-piano/)
 - [sampler](https://github.com/danigb/smplr)
 - [midi-note](https://www.npmjs.com/package/midi-note?activeTab=readme) [source](https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies)
-- [all-chords](https://www.pianote.com/blog/all-piano-chords/)
--- server/index.js
+++ server/index.js
@@ -1,24 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import midiToNote from 'midi-note'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
 
 import { types, typesLabels, roots } from './utils/consts.js'
 import { getMidiChord } from './utils/getMidiChord.js'
 
-import User from './models/User.js'
-
-import connectDB from './db.js'
-
 const PORT = /* process.env.PORT || */ 3001
 const app = express()
-const secretKey = 'commeundejavu2023'
 
 app.use(bodyParser.json())
-
-// connection to the MongoDB
-await connectDB()
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hi from api' })
@@ -55,77 +45,6 @@ app.get('/api/chord/:rootNote/:chordType', (req, res) => {
     midiChord,
     notes
   })
-})
-
-app.get('/api/tokenValidation', async (req, res) => {
-  const token = req.headers.authorization
-
-  if (!token) {
-    return res.status(401).json({ message: 'Token not provided' })
-  }
-
-  try {
-    const decodedToken = jwt.verify(token, secretKey)
-
-    const user = await User.findOne({ _id: decodedToken._id }, 'userName personnalChords')
-    res.json(user)
-  } catch (error) {
-    return res.status(403).json({ message: 'Token is invalid' })
-  }
-})
-
-app.post('/api/login', async (req, res) => {
-  const { userName, password } = req.body
-
-  try {
-    const userDb = await User.findOne({ userName }, 'userName password personnalChords')
-
-    if (!userDb) {
-      return res.status(400).json({ message: 'Invalid UserName or Password' })
-    }
-
-    const doesPasswordMatch = await bcrypt.compare(password, userDb.password)
-
-    if (!doesPasswordMatch) {
-      return res.status(401).json({ message: 'Invalid UserName or Password' })
-    }
-
-    const token = jwt.sign({ _id: userDb.id }, secretKey, {
-      expiresIn: '1h' // Token expiration time (adjust as needed)
-    })
-
-    const user = {
-      _id: userDb.id,
-      userName: userDb.userName,
-      personnalChords: userDb.personnalChords
-    }
-
-    res.json({ token, user })
-  } catch (error) {
-    console.error('Error while loging', error)
-  }
-})
-
-app.post('/api/register', async (req, res) => {
-  const { userName, password } = req.body
-  try {
-    const isUserNameTaken = await User.exists({ userName })
-
-    if (isUserNameTaken) {
-      return res.status(400).json({ message: 'UserName is alreay taken' })
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    await User.create({
-      userName,
-      password: hashedPassword
-    })
-
-    res.json({ message: 'User registered successfully' })
-  } catch (error) {
-    console.error('Error while registering new user', error)
-  }
 })
 
 app.listen(PORT, () => {
